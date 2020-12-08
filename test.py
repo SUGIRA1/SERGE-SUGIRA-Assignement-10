b815836e6cfa91a4f571096496f0183967a0d32d
SUGIRA1 <67726231+SUGIRA1@users.noreply.github.com>
2020-12-08 17:26:58 +0200

    Update test.py

--- test.py
+++ test.py
@@ -1,10 +1,19 @@
+from car_class import Car_class
 import unittest
-import car_class
-import 
+
+class MyTestCase(unittest.TestCase):
+
+ # Testing the car price        -------------       TEST 1
+
+    def test_car(self):
+        car1 = Car_class("Benz 2xs", "2012", "2014", "RAA454Y", "AVAILABLE", 100, 0, 0)
+        self.assertEqual(car1.prices, 100)
 
 
-# Testing The car addition ---------       TEST 1
-
-class BasicTest(unittest.TestCase):
-    def add_car(self):
-        self.assertEqual(car_class.add(1, 5), 6)
+        
+        
+        
+        
+        
+    if __name__ == '__main__':
+    unittest.main()
